ac506ee88634a518b06481c7304fbebf544fc97e
omniafathy1 <omniafathy779@gmail.com>
2025-03-26 19:13:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let moblie = document.getElementById("mobile-menu")
+
+let icon = document.querySelector(".icon")
+
+icon.addEventListener("click" , function(){
+    moblie.classList.toggle("hidden")
+})
